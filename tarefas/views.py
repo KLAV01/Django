e9efa811b525bcf518916fe02d1441786a160ac9
KLAV01 <kaiqueluan508@gmail.com>
2024-10-28 16:29:06 -0300

    update

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+def lista_tarefas(request):
+    tarefas = ["Estudar Django", "Criar um projeto", "Revisar o conteúdo"]
+    return render(request, 'lista_tarefas.html', {'tarefas': tarefas})
